6244b65c2acf46369694a309a9a7a5e7a9d49099
Rafael Azevedo <mail@rafaelazevedo.me>
2019-05-24 10:06:12 -0400

    fixed typo in file name

--- project.mk
+++ project.mk
@@ -2,5 +2,5 @@
 YAML_DIRECTORY?=deploy
 SELECTOR_SYNC_SET_TEMPLATE?=scripts/templates/selectorsyncset.yaml
 GIT_ROOT?=$(shell git rev-parse --show-toplevel 2>&1)
-SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/00-osd-manged-cluster-config.selectorsyncset.yaml
+SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/00-osd-managed-cluster-config.selectorsyncset.yaml
 GIT_HASH?=$(shell git rev-parse --short=7 HEAD 2>&1)
