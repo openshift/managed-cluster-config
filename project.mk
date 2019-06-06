4a5695dc65424ac7b849bcd6918a45d434b881d3
Naveen Malik <nmalik@redhat.com>
2019-06-06 12:16:26 -0400

    Update PR check to verify 'make' has been run

--- project.mk
+++ project.mk
@@ -3,4 +3,6 @@ YAML_DIRECTORY?=deploy
 SELECTOR_SYNC_SET_TEMPLATE_DIR?=scripts/templates/
 GIT_ROOT?=$(shell git rev-parse --show-toplevel 2>&1)
 SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/hack/00-osd-managed-cluster-config.selectorsyncset.yaml.tmpl
-REPO_NAME?=$(shell basename $$(git rev-parse --show-toplevel))
+
+# WARNING: REPO_NAME will default to the current directory if there are no remotes
+REPO_NAME?=$(shell basename $$((git config --get-regex remote\.*\.url 2>/dev/null || pwd) | head -n1 | sed 's|.git||g'))
