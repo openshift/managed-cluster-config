ea7e1505d60209a617e977e6774cc476fe7f3103
Naveen Malik <nmalik@redhat.com>
2019-06-11 16:58:02 -0400

    Fix REPO_NAME and use it to set SELECTOR_SYNC_SET_DESTINATION

--- project.mk
+++ project.mk
@@ -2,7 +2,8 @@
 YAML_DIRECTORY?=deploy
 SELECTOR_SYNC_SET_TEMPLATE_DIR?=scripts/templates/
 GIT_ROOT?=$(shell git rev-parse --show-toplevel 2>&1)
-SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/hack/00-osd-managed-cluster-config.selectorsyncset.yaml.tmpl
 
 # WARNING: REPO_NAME will default to the current directory if there are no remotes
-REPO_NAME?=$(shell basename $$((git config --get-regex remote\.*\.url 2>/dev/null || pwd) | head -n1 | sed 's|.git||g'))
+REPO_NAME?=$(shell basename $$((git config --get-regex remote\.*\.url 2>/dev/null | cut -d ' ' -f2 || pwd) | head -n1 | sed 's|.git||g'))
+
+SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/hack/00-osd-${REPO_NAME}.selectorsyncset.yaml.tmpl
