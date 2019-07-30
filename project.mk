88890500e89b6d88071a1852d5ee25cd23f1cf2b
alima <alima@redhat.com>
2019-07-30 09:39:20 -0400

    possible fix for Makefile failing in CI workflow

--- project.mk
+++ project.mk
@@ -4,7 +4,7 @@ SELECTOR_SYNC_SET_TEMPLATE_DIR?=scripts/templates/
 GIT_ROOT?=$(shell git rev-parse --show-toplevel 2>&1)
 
 # WARNING: REPO_NAME will default to the current directory if there are no remotes
-REPO_NAME?=$(shell basename $$((git config --get-regex remote\.*\.url 2>/dev/null | cut -d ' ' -f2 || pwd) | head -n1 | sed 's|.git||g'))
+REPO_NAME=managed-cluster-config
 
 SELECTOR_SYNC_SET_DESTINATION?=${GIT_ROOT}/hack/00-osd-${REPO_NAME}.selectorsyncset.yaml.tmpl
 
