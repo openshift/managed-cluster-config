ea62825dd24687b3f19ec6b35c47514da59e536a
Christopher Collins <collins.christopher@gmail.com>
2019-08-27 13:19:28 -0400

    Update scripts/cluster-upgrade.sh
    Add comment about non-numeric versions
    
    Co-Authored-By: Naveen Malik <jewzaam@gmail.com>

--- scripts/cluster-upgrade.sh
+++ scripts/cluster-upgrade.sh
@@ -66,6 +66,7 @@ vercomp() {
     do
         ver1[i]=0
     done
+    # NOTE any non-numeric versions that are not identical will fail in the following loop, it expects numeric components to all versions
     for ((i=0; i<${#ver1[@]}; i++))
     do
         if [[ -z ${ver2[i]} ]]
