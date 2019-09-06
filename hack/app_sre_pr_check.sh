751bc3c989dcb7ce70eeab192339b355ea622dde
Naveen Malik <nmalik@redhat.com>
2019-09-06 08:37:13 -0400

    Add namespace to all SRE prometheus alerts
    https://jira.coreos.com/browse/SREP-2065
    
    And added PR check to make sure any alert added has a namespace defined.

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -2,6 +2,25 @@
 
 set -exv
 
+# all custom alerts must have a namespace label
+MISSING_NS="false"
+for F in $(ls deploy/sre-prometheus)
+do
+    # requires yq
+    MISSING_NS_COUNT=$(yq '.spec.groups[].rules[] | select(.namespace == null) and select(.labels.namespace == null)' deploy/sre-prometheus/$F | wc -l)
+
+    if [ "$MISSING_NS_COUNT" != "0" ]
+    then
+        echo "Rule missing 'namespace' in file '$F'"
+        MISSING_NS="true"
+    fi
+done
+
+if [ "$MISSING_NS" == "true" ]
+then
+    exit 2
+fi
+
 # if running `make` changes anything, fail the build
 # order is inconsistent across systems, sort the template file.. it's not perfect but it's better than nothing
 cat hack/00-osd-managed-cluster-config.selectorsyncset.yaml.tmpl | sort > sorted-before.yaml.tmpl
