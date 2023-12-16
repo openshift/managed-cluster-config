3f64cd149dd93fd1373914b52f2c09b0c6751d66
Joshua Packer <jpacker@redhat.com>
2023-12-16 00:37:28 -0500

    Use extraDependencies to include a check for the ingress CustomResource. * Update PolicyGenerator to 1.12.3 (to support extraDependencies * Fix ClusterSelectors with quotes * Add new Policy to look for ingress custom resource * Add extraDependencies on the new Policy (must Comply before the TLS certificates are configured) * Fixed a grep in generate-policy.sh, that would create an error, when extraDependencies was used.
    Signed-off-by: Joshua Packer <jpacker@redhat.com>

--- scripts/generate-policy.sh
+++ scripts/generate-policy.sh
@@ -6,7 +6,7 @@ ROOT_DIR=$PWD
 for dir in $DIRECTORY; do
     echo $dir
     cd $dir
-    name=$(grep "\- name:" $FILENAME | cut -d: -f2- | xargs)
+    name=$(grep "^\- name:" $FILENAME | cut -d: -f2- | xargs)
     echo $name
     PolicyGenerator $FILENAME > $ROOT_DIR/deploy/acm-policies/50-GENERATED-$name.Policy.yaml
     cd $ROOT_DIR
