2010358dc9b6b0754e6342dab96b1b2d3913dd1d
Claudio Busse <cbusse@redhat.com>
2024-01-23 16:43:33 +0100

    OSD-19863: Lower retention for management cluster CMO prometheus data

--- scripts/generate-cmo-config.py
+++ scripts/generate-cmo-config.py
@@ -5,8 +5,14 @@ import os
 
 input_file_path = os.path.join("resources", "cluster-monitoring-config", "config.yaml")
 output_file_path_non_uwm = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "50-GENERATED-cluster-monitoring-config.yaml")
+output_file_path_non_uwm_4_5 = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "clusters-v4.5", "50-GENERATED-cluster-monitoring-config.yaml")
+
 output_file_path_uwm = os.path.join("deploy", "cluster-monitoring-config", "50-GENERATED-cluster-monitoring-config.yaml")
 output_file_path_fr = os.path.join("deploy", "osd-fedramp-cluster-monitoring-config", "50-GENERATED-cluster-monitoring-config.yaml")
+
+input_mc_file_path = os.path.join("resources", "cluster-monitoring-config", "management-clusters", "config.yaml")
+output_mc_file_path_non_uwm = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "management-clusters", "50-GENERATED-cluster-monitoring-config.yaml")
+output_mc_file_path_uwm = os.path.join("deploy", "cluster-monitoring-config", "management-clusters", "50-GENERATED-cluster-monitoring-config.yaml")
 
 
 def str_presenter(dumper, data):
@@ -16,8 +22,8 @@ def str_presenter(dumper, data):
 
 yaml.add_representer(str, str_presenter)
 
-def dump_configmap(configmap_path, enableUserWorkload, disableremoteWrite):
-    with open(input_file_path,'r') as input_file:
+def dump_configmap(input_path, configmap_path, enableUserWorkload, disableremoteWrite):
+    with open(input_path,'r') as input_file:
         config = yaml.safe_load(input_file)
         config["enableUserWorkload"] = enableUserWorkload
         if disableremoteWrite:
@@ -37,6 +43,9 @@ def dump_configmap(configmap_path, enableUserWorkload, disableremoteWrite):
         with open(configmap_path, 'w') as outfile:
             yaml.dump(cmo_config, outfile)
 
-dump_configmap(output_file_path_uwm, True, False)
-dump_configmap(output_file_path_non_uwm, False, False)
-dump_configmap(output_file_path_fr, True, True)
+dump_configmap(input_file_path, output_file_path_uwm, True, False)
+dump_configmap(input_file_path, output_file_path_non_uwm, False, False)
+dump_configmap(input_file_path, output_file_path_non_uwm_4_5, False, False)
+dump_configmap(input_file_path, output_file_path_fr, True, True)
+dump_configmap(input_mc_file_path, output_mc_file_path_uwm, True, False)
+dump_configmap(input_mc_file_path, output_mc_file_path_non_uwm, False, False)
