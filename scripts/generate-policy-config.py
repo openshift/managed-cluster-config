cba2576d82eb48e990b2c16b73ec8499eb26d690
Kirk Bater <kirk.bater@gmail.com>
2025-08-07 16:07:10 -0400

    Revert "Add rosa-legacy-brand-migration cronjob with priority class and resource requests"

--- scripts/generate-policy-config.py
+++ scripts/generate-policy-config.py
@@ -41,7 +41,6 @@ directories = [
         'rbac-permissions-operator-config',
         'rosa-console-branding',
         'rosa-console-legacy-branding-configmap',
-        'rosa-console-legacy-branding-removal',
         'rosa-ingress-certificate-policies',
         'rosa-ingress-certificate-check',
         ]
