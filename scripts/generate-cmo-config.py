ce1475cb966e9938b66b04eb4aa93788754d0239
Jan Fajerski <jfajersk@redhat.com>
2025-09-08 10:58:29 +0200

    CMO: Prometheus Adapter was deprecated in 4.17
    The Monitoring team wants to remove the `k8sPrometheusAdapter` config
    so adjust the OSD configs for 4.17 and later.
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

--- scripts/generate-cmo-config.py
+++ scripts/generate-cmo-config.py
@@ -8,8 +8,10 @@ input_file_path = os.path.join("resources", "cluster-monitoring-config", "config
 output_file_path_non_uwm = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "50-GENERATED-cluster-monitoring-config.yaml")
 output_file_path_non_uwm_4_5 = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "clusters-v4.5", "50-GENERATED-cluster-monitoring-config.yaml")
 output_file_path_non_uwm_pre_411 = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "pre-4.11", "50-GENERATED-cluster-monitoring-config.yaml")
+output_file_path_non_uwm_411_416 = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "4.11-4.16", "50-GENERATED-cluster-monitoring-config.yaml")
 output_file_path_uwm = os.path.join("deploy", "cluster-monitoring-config", "50-GENERATED-cluster-monitoring-config.yaml")
 output_file_path_uwm_pre_411 = os.path.join("deploy", "cluster-monitoring-config", "pre-4.11", "50-GENERATED-cluster-monitoring-config.yaml")
+output_file_path_uwm_411_416 = os.path.join("deploy", "cluster-monitoring-config", "4.11-4.16", "50-GENERATED-cluster-monitoring-config.yaml")
 output_file_path_fr = os.path.join("deploy", "osd-fedramp-cluster-monitoring-config", "50-GENERATED-cluster-monitoring-config.yaml")
 output_mc_file_path_non_uwm = os.path.join("deploy", "cluster-monitoring-config-non-uwm", "management-clusters", "50-GENERATED-cluster-monitoring-config.yaml")
 output_mc_file_path_uwm = os.path.join("deploy", "cluster-monitoring-config", "management-clusters", "50-GENERATED-cluster-monitoring-config.yaml")
@@ -23,17 +25,21 @@ yaml.add_representer(str, str_presenter)
 
 def dump_configmap(input_path, configmap_path, enableUserWorkload,
                    disableremoteWrite, retentionTime = "11d",
-                   enableGrafana = False):
+                   enableGrafana = False,
+                   keepPrometheusAdapter = False):
     with open(input_path,'r') as input_file:
         config = yaml.safe_load(input_file)
         config["enableUserWorkload"] = enableUserWorkload
         config["prometheusK8s"]["retention"] = retentionTime
         if disableremoteWrite:
-           del config['prometheusK8s']['remoteWrite']
+            del config['prometheusK8s']['remoteWrite']
 
         if enableGrafana:
             # Grafana config was removed in 4.11, pre-4.11, put it back
             config["grafana"] = copy.deepcopy(config["prometheusOperator"])
+
+        if not keepPrometheusAdapter:
+            del config['k8sPrometheusAdapter']
 
         cmo_config = {
             "apiVersion": "v1",
@@ -52,10 +58,12 @@ def dump_configmap(input_path, configmap_path, enableUserWorkload,
             yaml.dump(cmo_config, outfile)
 
 dump_configmap(input_file_path, output_file_path_uwm, True, False)
-dump_configmap(input_file_path, output_file_path_uwm_pre_411, True, False, "7d", True)
+dump_configmap(input_file_path, output_file_path_uwm_pre_411, True, False, "7d", True, True)
+dump_configmap(input_file_path, output_file_path_uwm_411_416, True, False, "7d", True, True)
 dump_configmap(input_file_path, output_file_path_non_uwm, False, False)
-dump_configmap(input_file_path, output_file_path_non_uwm_4_5, False, False, "11d", True)
-dump_configmap(input_file_path, output_file_path_non_uwm_pre_411, False, False, "7d", True)
+dump_configmap(input_file_path, output_file_path_non_uwm_4_5, False, False, "11d", True, True)
+dump_configmap(input_file_path, output_file_path_non_uwm_pre_411, False, False, "7d", True, True)
+dump_configmap(input_file_path, output_file_path_non_uwm_411_416, False, False, "7d", True, True)
 dump_configmap(input_file_path, output_file_path_fr, True, True)
 dump_configmap(input_file_path, output_mc_file_path_uwm, True, False, "7d")
 dump_configmap(input_file_path, output_mc_file_path_non_uwm, False, False, "7d")
